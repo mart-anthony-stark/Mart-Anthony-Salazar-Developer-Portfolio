d240578fd2d26301f5bb31e5fd36fdf46b85ecd8
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-22 22:03:50 +0800

    Add: active link classname

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,31 @@
 import styles from "../../styles/navbar.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <nav className={styles.nav}>
       <div className={styles.left}>
-        <h1>M</h1>
+        <h1 className={styles.logo}>M</h1>
+        <div className={styles.name}>
+          <h2>Mart Salazar</h2>
+          <span>Fullstack Web Developer</span>
+        </div>
       </div>
       <div className={styles.links}>
-        <Link href="/">Home</Link>
-        <Link href="#about">About</Link>
-        <Link href="#works">Works</Link>
-        <Link href="#">Contact</Link>
+        <li className={router.asPath == "/" ? "active" : ""}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={router.asPath == "/#about" ? "active" : ""}>
+          <Link href="#about">About</Link>
+        </li>
+        <li className={router.asPath == "/#works" ? "active" : ""}>
+          <Link href="#works">Works</Link>
+        </li>
+        <li className={router.asPath == "/#contact" ? "active" : ""}>
+          <Link href="/#contact">Contact</Link>
+        </li>
       </div>
     </nav>
   );
