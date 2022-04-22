74847fdc4b6a0e36a650e08ef276f2f7d870a79c
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-22 23:31:02 +0800

    Add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import styles from "../styles/Home.module.scss";
 const Home: NextPage = () => {
   return (
     <div>
-      <Navbar />
+      <section className={styles.hero}></section>
     </div>
   );
 };
