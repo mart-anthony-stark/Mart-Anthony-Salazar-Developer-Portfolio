afd0b6b36cab196adcea99b3ec9daae67187ef6c
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-23 13:30:38 +0800

    Add: typography animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,33 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/navbar/Navbar";
 import styles from "../styles/Home.module.scss";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <section className={styles.hero}></section>
+      <section className={styles.hero}>
+        <motion.h1
+          animate={{ y: -100, opacity: 0 }}
+          transition={{ delay: 2, duration: 1.3 }}
+          className={styles.typography}
+        >
+          <motion.span
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            Turning Ideas
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            Into Solutions
+          </motion.span>
+        </motion.h1>
+      </section>
     </div>
   );
 };
