0b5bcf18f180b5a04df4ab8fbc48feba4286f7ae
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-03 00:13:19 +0800

    Add: typography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../layouts/Layout";
+import Typography from "../components/Typography";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
