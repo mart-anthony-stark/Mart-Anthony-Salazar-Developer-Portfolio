aac6fb81dac9b75c1e61d3672285be16f1d06e5c
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-13 22:12:39 +0800

    Add: social links

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -4,21 +4,25 @@ import style from "../styles/socials.module.scss";
 const Social = () => {
   return (
     <div className={style.socials}>
-      <Icon icon="akar-icons:github-fill" color="#ccd6f6" height={30} />
+      <a href="https://github.com/mart-anthony-stark">
+        <Icon icon="akar-icons:github-fill" color="#ccd6f6" height={30} />
+      </a>
       <span></span>
-      <Icon icon="akar-icons:facebook-fill" color="#ccd6f6" height={30} />
+      <a href="https://www.facebook.com/profile.php?id=100081564911565">
+        <Icon icon="akar-icons:facebook-fill" color="#ccd6f6" height={30} />
+      </a>
       <span></span>
-      <Icon
-        icon="ant-design:codepen-circle-filled"
-        color="#ccd6f6"
-        height={30}
-      />
+      <a href="https://codepen.io/martanthonystark404">
+        <Icon
+          icon="ant-design:codepen-circle-filled"
+          color="#ccd6f6"
+          height={30}
+        />
+      </a>
       <span></span>
-      <Icon
-        icon="ant-design:twitter-circle-filled"
-        color="#ccd6f6"
-        height={30}
-      />
+      <a href="https://www.linkedin.com/in/mart-salazar/">
+        <Icon icon="ant-design:linkedin-filled" color="#ccd6f6" height={30} />
+      </a>
     </div>
   );
 };
