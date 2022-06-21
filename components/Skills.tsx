ca48e2e97171b9f889836dee7475dbb6948881ca
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 22:16:51 +0800

    Add: skills

---
+++ components/Skills.tsx
@@ -0,0 +1,11 @@
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <div className="icons">
+          
+      </div>
+    </div>
+  );
+};
+export default Skills;
