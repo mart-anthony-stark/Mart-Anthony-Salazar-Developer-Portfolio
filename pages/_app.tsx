2f1f26c75d7d3064410fe3491a247f4ccef69cb9
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-21 21:39:15 +0800

    Add: cursor animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,35 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../layouts/Layout";
 import Typography from "../components/Typography";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const cursor: any = document.querySelector("#cursor");
+    function followCursor(e: MouseEvent) {
+      cursor.style.top = e.clientY + window.scrollY + "px";
+      cursor.style.left = e.clientX + "px";
+    }
+    function snap() {
+      cursor.classList.add("snap");
+
+      setTimeout(() => {
+        cursor.classList.remove("snap");
+      }, 500);
+    }
+
+    window.addEventListener("mousemove", followCursor);
+    window.addEventListener("click", snap);
+
+    return () => {
+      window.removeEventListener("mousemove", followCursor);
+      window.removeEventListener("click", snap);
+    };
+  }, []);
+
   return (
     <Layout>
+      <div id="cursor"></div>
       <Component {...pageProps} />
     </Layout>
   );
