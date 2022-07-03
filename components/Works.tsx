50bda36e09b8f1d099690125e7061e1efe7fa68d
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-03 20:39:29 +0800

    add: project

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,19 +1,29 @@
 import styles from "../styles/works.module.scss";
 import Image from "next/image";
 import reactiveOS from "../public/projects/react-os.webp";
+import pizza from "../public/projects/Pizza.webp";
+import Link from "next/link";
 
 const Works = () => {
   return (
     <section id="works" className={styles.works}>
       <div className={styles.headerGrid}>
-        <div className={`${styles.block} ${styles.b1}`}>
-          <Image src={reactiveOS} />
+        <Link href="/">
+          <div className={`${styles.block} ${styles.b1}`}>
+            <Image src={reactiveOS} />
+            <div className={styles.desc}>
+              <h2>Reactive OS</h2>
+              <h4>A desktop environment UI built using ReactJS</h4>
+            </div>
+          </div>
+        </Link>
+        <div className={`${styles.block} ${styles.b2}`}>
+          <Image src={pizza} />
           <div className={styles.desc}>
-            <h2>Reactive OS</h2>
-            <h4>A desktop environment UI built using ReactJS</h4>
+            <h2>Pizza Arc</h2>
+            <h4>Pizza Ordering Web App using Node.js</h4>
           </div>
         </div>
-        <div className={`${styles.block} ${styles.b2}`}></div>
         <div className={`${styles.block} ${styles.b3}`}></div>
         <div className={`${styles.block} ${styles.b4}`}></div>
         <div className={styles["bottom-block"]}>
