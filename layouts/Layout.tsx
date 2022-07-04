a59ee4cfaaae0c651253e025144d66ada8fe6d35
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-04 20:03:19 +0800

    add: app context

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,19 +1,15 @@
 import { useRouter } from "next/router";
-import { FC, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Social from "../components/Socials";
+import AppContext from "../context/AppContext";
 
 type Props = {
   children: any;
 };
 
 const Layout: FC<Props> = ({ children }) => {
-  const router = useRouter();
-  const [activeRoute, setActiveRoute] = useState("");
-
-  useEffect(() => {
-    setActiveRoute(router.asPath);
-  }, [router.asPath]);
+  const { activeRoute, setActiveRoute } = useContext(AppContext);
 
   return (
     <div>
