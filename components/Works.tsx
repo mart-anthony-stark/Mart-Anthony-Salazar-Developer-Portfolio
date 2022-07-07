3ee14867a4c02b56ff235ca3899189e2d3858022
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-07 20:51:42 +0800

    add: dynamic rendering

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,20 +1,49 @@
 import styles from "../styles/works.module.scss";
 import Image from "next/image";
-import reactiveOS from "../public/projects/react-os.webp";
-import pizza from "../public/projects/Pizza.webp";
-import shrewsburry from "../public/projects/shrewsburry.webp";
-import ShoeShop from "../public/projects/ShoeShop.webp";
+// import reactiveOS from "../public/projects/reactos/react-os.webp";
+// import pizza from "../public/projects/Pizza.webp";
+// import shrewsburry from "../public/projects/shrewsburry.webp";
+// import ShoeShop from "../public/projects/ShoeShop.webp";
+import projects from "../public/projects.json";
 
 import Link from "next/link";
 
+const blockClassNames = [
+  styles.b1,
+  styles.b2,
+  styles.b3,
+  styles.b4,
+  styles.b5,
+  styles.b6,
+  styles.b7,
+];
 const Works = () => {
   return (
     <section id="works" className={styles.works}>
       <h1 className="heading">Works</h1>
       <div className={styles.headerGrid}>
-        <Link href="/">
+        {projects.map((project, index) => (
+          <Link href="/">
+            <div className={`${styles.block} ${blockClassNames[index]}`}>
+              <Image
+                alt={project["images"][0]["alt"]}
+                src={project["images"][0]["src"]}
+                layout="fill"
+              />
+              <div className={styles.desc}>
+                <h2>{project["title"]}</h2>
+                <h4>{project["subheading"]}</h4>
+              </div>
+            </div>
+          </Link>
+        ))}
+        {/* <Link href="/">
           <div className={`${styles.block} ${styles.b1}`}>
-            <Image alt="Reactive OS" src={reactiveOS} />
+            <Image
+              alt="Reactive OS"
+              src="https://raw.githubusercontent.com/mart-anthony-stark/Mart-Anthony-Salazar-Developer-Portfolio/main/public/projects/reactos/react-os.webp"
+              layout="fill"
+            />
             <div className={styles.desc}>
               <h2>Reactive OS</h2>
               <h4>A desktop environment UI built using ReactJS</h4>
@@ -39,14 +68,14 @@ const Works = () => {
           <Image alt="Student Attendance System with QR" src={shrewsburry} />
           <div className={styles.desc}>
             <h2>Shrewsbury International School</h2>
-            <h4>School Attendance System with QR code using Vue and Node.js</h4>
+            <h4>  </h4>
           </div>
         </div>
         <div className={styles["bottom-block"]}>
           <div className={`${styles.block} ${styles.b5}`}></div>
           <div className={`${styles.block} ${styles.b6}`}></div>
           <div className={`${styles.block} ${styles.b7}`}></div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
