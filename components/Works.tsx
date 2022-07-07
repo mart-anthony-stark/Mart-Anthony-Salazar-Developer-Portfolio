083f26befd27c729f9b4018789611b28f6ab3c38
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-07 21:00:35 +0800

    remove unnecessary

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,9 +1,5 @@
 import styles from "../styles/works.module.scss";
 import Image from "next/image";
-// import reactiveOS from "../public/projects/reactos/react-os.webp";
-// import pizza from "../public/projects/Pizza.webp";
-// import shrewsburry from "../public/projects/shrewsburry.webp";
-// import ShoeShop from "../public/projects/ShoeShop.webp";
 import projects from "../public/projects.json";
 
 import Link from "next/link";
@@ -37,45 +33,6 @@ const Works = () => {
             </div>
           </Link>
         ))}
-        {/* <Link href="/">
-          <div className={`${styles.block} ${styles.b1}`}>
-            <Image
-              alt="Reactive OS"
-              src="https://raw.githubusercontent.com/mart-anthony-stark/Mart-Anthony-Salazar-Developer-Portfolio/main/public/projects/reactos/react-os.webp"
-              layout="fill"
-            />
-            <div className={styles.desc}>
-              <h2>Reactive OS</h2>
-              <h4>A desktop environment UI built using ReactJS</h4>
-            </div>
-          </div>
-        </Link>
-        <div className={`${styles.block} ${styles.b2}`}>
-          <Image alt="Pizza Web App" src={pizza} />
-          <div className={styles.desc}>
-            <h2>Pizza Arc</h2>
-            <h4>Pizza Ordering Web App using Node.js</h4>
-          </div>
-        </div>
-        <div className={`${styles.block} ${styles.b3}`}>
-          <Image alt="Footwear Ecommerce" src={ShoeShop} />
-          <div className={styles.desc}>
-            <h2>Shoe Shop</h2>
-            <h4>A desktop environment UI built using ReactJS</h4>
-          </div>
-        </div>
-        <div className={`${styles.block} ${styles.b4}`}>
-          <Image alt="Student Attendance System with QR" src={shrewsburry} />
-          <div className={styles.desc}>
-            <h2>Shrewsbury International School</h2>
-            <h4>  </h4>
-          </div>
-        </div>
-        <div className={styles["bottom-block"]}>
-          <div className={`${styles.block} ${styles.b5}`}></div>
-          <div className={`${styles.block} ${styles.b6}`}></div>
-          <div className={`${styles.block} ${styles.b7}`}></div>
-        </div> */}
       </div>
     </section>
   );
