d6f500d251c8c8c8ed2c41361d85422cafba7293
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-11 20:36:40 +0800

    fixed(index): remove Found unused variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/navbar/Navbar";
 import styles from "../styles/Home.module.scss";
-import { motion } from "framer-motion";
 import Typography from "../components/Typography";
 import About from "../components/about";
 import Works from "../components/Works";
 import Social from "../components/Socials";
-
-const variants = {
-  visible: { opacity: 1, y: 0 },
-  hidden: { opacity: 0, y: 100 },
-};
 
 const Home: NextPage = () => {
   return (
