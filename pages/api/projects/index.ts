78ee047e6a377dd55f66b19103785c47010e8ed6
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-11 20:32:02 +0800

    fixed: json

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Project } from "../../../types";
+import projects from "../../../public/projects.json";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Project[]>
 ) {
-  const projects = [{ id: 1, title: "iaugsasdasdasdasdad" }];
   res.status(200).json(projects);
 }
