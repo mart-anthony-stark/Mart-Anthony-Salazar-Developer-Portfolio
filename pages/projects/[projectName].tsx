877b23b27fd558d481f9badf455d19cca9985ed5
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-13 19:52:04 +0800

    add: image

--- pages/projects/[projectName].tsx
+++ pages/projects/[projectName].tsx
@@ -14,10 +14,28 @@ const Project: FC<Props> = (props) => {
     <div className={styles.project}>
       <div className={styles["image-container"]}>
         <h2>{project.title}</h2>
-        <div className={styles["main"]}>
-          <img src={project.images[0]["src"]} alt={project.images[0]["alt"]} />
+        <div className={styles["two-cols"]}>
+          <div className={styles["main"]}>
+            <Image
+              src={project.images[0]["src"]}
+              alt={project.images[0]["alt"]}
+              height={300}
+              width={600}
+            />
+          </div>
+          <div className={styles["menu"]}>
+            {project.images.map((image) => (
+              <div className={`${styles["button"]} ${styles["active"]}`}>
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  height={100}
+                  width={200}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className={styles["menu"]}></div>
       </div>
     </div>
   );
