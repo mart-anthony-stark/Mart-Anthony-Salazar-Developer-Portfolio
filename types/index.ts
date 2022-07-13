fedbde70618066778b577b37299a8e2d3071abad
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-13 19:00:34 +0800

    add imageobj type

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,12 @@
+export type ImageObj = {
+  src: string;
+  alt?: string;
+};
+
 export type Project = {
   title: string;
   subheading: string;
   link: string;
   sourcecode: string;
-  images: Object[]
+  images: ImageObj[];
 };
